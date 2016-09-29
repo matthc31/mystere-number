378252aa57afcfc45d9d698427f50e7f52f0be00
Maxime Pawlak <contact@maximepawlak.fr>
2016-09-29 11:03:27 +0200

    Add README and step1

---
+++ nombreMystere/index-step1.js
@@ -0,0 +1,27 @@
+$(document).ready(main);
+
+// Fonction principale
+function main(){
+	
+	// == Fonction clickValider == 
+	// Créer la fonction 'clickValider'
+
+		// Récupérer le contenu de mon input
+
+		// Comparer ce contenu à un nombre que vous choisissez
+			// Si le contenu est égal
+			// Afficher une alert Gagné
+
+			// Sinon,
+			// Afficher une alert Perdu
+
+
+	// /Fin == Fonction clickValider == 
+
+
+
+	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
+	// Et qui appelle la fonction 'clickValider'
+
+
+}
