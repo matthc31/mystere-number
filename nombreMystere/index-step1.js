fadde84d0055c6606076b871f7bb8778e3f3c716
matthc <matthieucesbron@gmail.com>
2016-10-04 16:56:35 +0200

    trouver le nombre mystere

--- nombreMystere/index-step1.js
+++ nombreMystere/index-step1.js
@@ -5,23 +5,35 @@ function main(){
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
+	function clickValider(){
+		var taper = $('input').val();
 
 		// Récupérer le contenu de mon input
 
 		// Comparer ce contenu à un nombre que vous choisissez
 			// Si le contenu est égal
+			if(taper === 0){
 			// Afficher une alert Gagné
+			alert("Gagné");
+		}
 
+			
 			// Sinon,
 			// Afficher une alert Perdu
 
-
-	// /Fin == Fonction clickValider == 
+			else {
+				alert("Perdu!");
+			}
+	}// /Fin == Fonction clickValider == 
 
 
 
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
 
-
+	$("button").click(function(){
+		clickValider()
+	});
 }
+
+
--- nombreMystere/index-step2.js
+++ nombreMystere/index-step2.js
@@ -1,32 +1,48 @@
-$(document).ready(pied);
+$(document).ready(main);
 
 // Fonction principale
-function pied(){
+function main(){
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
+	// Récupérer le contenu de mon input
+	function clickValider(){
+		var taper = $('input').val();
 
-		// Récupérer le contenu de mon input
+
 
 		// Stocker un nombre ENTIER généré "aléatoirement"
-
+		var nbr = Math.round(Math.random() * 3);
+		console.log(nbr)
 		// Comparer ce contenu à ce nombre
 			// Si le contenu est égal
+			if(taper == nbr){
 			// Afficher une alert Gagné
+			alert("Gagné");	
+		}
 
 			// Sinon s'il est supérieur
+			else if(taper>nbr){
+				alert("c'est balo c'est trop grand!");	
+			}
 			// Afficher une alert Perdu, votre nombre est trop grand
-
+			
 			// Sinon s'il est inférieur
+			else{
+				alert("c'est balo c'est trop petit!");
+			}
 			// Afficher une alert Perdu, votre nombre est trop petit
-			
-			
+
+		}
+
 	// /Fin == Fonction clickValider == 
 
 
 
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
+	$("button").click(function(){
+		clickValider()
+	});
 }
+
